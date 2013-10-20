205b3e077910879ed8c804decf1e4c5199151b68
Andries Coetzee <andriesc@lime-square.net>
2013-10-20 22:59:06 +0200

    started

---
+++ public/lib/google.js
@@ -0,0 +1,16 @@
+//Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setDomainName', 'none']);
+_gaq.push(['_setAccount', 'UA-23945786-3']);
+//_gaq.push(['_trackPageview']);
+
+(function () {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  var debug = false;
+  var filename = debug ? 'u/ga_debug.js' : 'ga.js';
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/' + filename;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
