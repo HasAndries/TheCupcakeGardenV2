0ee4965401a4be1fed43405b856add928a882206
Andries Coetzee <andriesc@lime-square.net>
2013-11-24 14:32:10 +0200

    gallery is functional

--- public/js/app.js
+++ public/js/app.js
@@ -3,4 +3,4 @@ var tcgServices = angular.module('tcg.services', []);
 var tcgFilters = angular.module('tcg.filters', []);
 var tcgDirectives = angular.module('tcg.directives', []);
 
-var tcg = angular.module('tcg', ['tcg.config', 'tcg.services', 'tcg.filters', 'tcg.directives', 'ui.bootstrap']).run();
+var tcg = angular.module('tcg', ['tcg.config', 'tcg.services', 'tcg.filters', 'tcg.directives']).run();
