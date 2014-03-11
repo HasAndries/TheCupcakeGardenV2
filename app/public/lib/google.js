030c838d77dacca1852db95d92370e6c9efe41d8
Andries Coetzee <andriesc@lime-square.net>
2014-03-11 08:02:28 +0200

    new analytics script

--- app/public/lib/google.js
+++ app/public/lib/google.js
@@ -1,17 +1,7 @@
-//Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setDomainName', 'none']);
-_gaq.push(['_setAccount', 'UA-23945786-3']);
-_gaq.push(['_trackPageview']);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-(function () {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  var debug = false;
-  var filename = debug ? 'u/ga_debug.js' : 'ga.js';
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/' + filename;
-  //ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
+ga('create', 'UA-23945786-3', 'thecupcakegarden.co.za');
+ga('send', 'pageview');
