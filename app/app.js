d697b354a0a628b6ffc65670e9e9e485366344c1
Andries Coetzee <andriesc@lime-square.net>
2014-08-21 20:48:57 +0200

    added gzip and cache

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var swig = require('swig');
 var path = require('path');
 var Pages = require('./routes/pages');
 
-function Admin(config){
+function Admin(config) {
   var _this = this;
   _this.config = config;
 
@@ -17,8 +17,10 @@ function Admin(config){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.compress());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public'))); //public
+  var oneDay = 86400000;
+  app.use(express.static(path.join(__dirname, 'public'), {maxAge: oneDay})); //public
   //views
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'html');
@@ -30,7 +32,7 @@ function Admin(config){
   //routes
   _this.pages = new Pages(app, config);
 }
-Admin.prototype.start = function(http){
+Admin.prototype.start = function (http) {
   var _this = this;
   http.createServer(_this.app).listen(_this.app.get('port'), function () {
     console.log('TheCupcakeGarden server listening on port ' + _this.app.get('port'));
